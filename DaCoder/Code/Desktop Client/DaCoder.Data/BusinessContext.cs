6e1ceca53b34fde42efd22ffb4de46dff252a4f9
Simon <simonjonpedersen@gmail.com>
2015-11-12 12:55:12 +0100

    SQLite & EF
    Added SQLite with Entity Framework support

---
+++ DaCoder/Code/Desktop Client/DaCoder.Data/BusinessContext.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+namespace DaCoder.Data
+{
+    public class BusinessContext : IDisposable
+    {
+        private readonly DataContext dataContext;
+        private bool disposed;
+
+        public BusinessContext()
+        {
+            dataContext = new DataContext();
+        }
+
+        public DataContext DataContext
+        {
+            get { return dataContext; }
+        }
+
+        public TestModel AddNewTestModel(String name)
+        {
+            if (name == null)
+                throw new ArgumentNullException("name", "name must be not-null.");
+
+            if (String.IsNullOrEmpty(name))
+                throw new ArgumentException("name must not be an empty string.", "name");
+
+            var testModel = new TestModel
+            {
+                Name = name,
+            };
+
+            dataContext.TestModels.Add(testModel);
+            dataContext.SaveChanges();
+
+            return testModel;
+        }
+
+        #region IDisposable members
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        private void Dispose(bool disposing)
+        {
+            if (disposed || !disposing)
+                return;
+
+            if (dataContext != null)
+                dataContext.Dispose();
+
+            disposed = true;
+        }
+
+        #endregion
+
+    }
+}
---
+++ DaCoder/Code/Desktop Client/DaCoder.Data/DataContext.cs
@@ -0,0 +1,13 @@
+﻿using System.Data.Entity;
+
+namespace DaCoder.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext() : base("DefaultSQLite")
+        {
+        }
+
+        public DbSet<TestModel> TestModels { get; set; }
+    }
+}
