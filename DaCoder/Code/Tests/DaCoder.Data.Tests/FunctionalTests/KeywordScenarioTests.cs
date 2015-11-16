a2234b8314a782853bcf2e8aecda9614e253abbc
Simon <simonjonpedersen@gmail.com>
2015-11-16 11:41:04 +0100

    Added model features
    Added model features as: UpdateLanguage, DeleteLanguage, UpdateKeyword
    and DeleteKeyword

--- DaCoder/Code/Tests/DaCoder.Data.Tests/FunctionalTests/KeywordScenarioTests.cs
+++ DaCoder/Code/Tests/DaCoder.Data.Tests/FunctionalTests/KeywordScenarioTests.cs
@@ -7,7 +7,7 @@ namespace DaCoder.Data.Tests.FunctionalTests
     public class KeywordScenarioTests : FunctionalTest
     {
         [TestMethod]
-        public void AddNewKeyWordIsPersisted()
+        public void AddNewKeywordIsPersisted()
         {
             using (var businessContext = new BusinessContext())
             {
@@ -26,9 +26,67 @@ namespace DaCoder.Data.Tests.FunctionalTests
 
                 businessContext.AddNewKeyword(keyword);
 
-                bool exists = businessContext.DataContext.Languages.Any(l => l.Id == keyword.Id);
+                bool exists = businessContext.DataContext.Languages.Any(k => k.Id == keyword.Id);
 
                 Assert.IsTrue(exists);
+            }
+        }
+
+        [TestMethod]
+        public void UpdateKeywordIsPersisted()
+        {
+            using (var businessContext = new BusinessContext())
+            {
+                var language = new Language
+                {
+                    Name = "LanguageName"
+                };
+
+                businessContext.AddNewLanguage(language);
+
+                var keyword = new Keyword
+                {
+                    Name = "KeywordName",
+                    LanguageId = 1,
+                };
+
+                businessContext.AddNewKeyword(keyword);
+
+                keyword.Name = "New KeywordName";
+
+                businessContext.UpdateKeyword(keyword);
+
+                bool updated = businessContext.DataContext.Keywords.Any(k => k.Id == keyword.Id && k.Name == keyword.Name);
+
+                Assert.IsTrue(updated);
+            }
+        }
+
+        [TestMethod]
+        public void DeleteKeywordIsPersisted()
+        {
+            using (var businessContext = new BusinessContext())
+            {
+                var language = new Language
+                {
+                    Name = "LanguageName"
+                };
+
+                businessContext.AddNewLanguage(language);
+
+                var keyword = new Keyword
+                {
+                    Name = "KeywordName",
+                    LanguageId = 1,
+                };
+
+                businessContext.AddNewKeyword(keyword);
+
+                //businessContext.DeleteKeyword(keyword);
+
+                bool notDeleted = businessContext.DataContext.Keywords.Any(k => k.Id == keyword.Id);
+
+                //Assert.IsFalse(notDeleted);
             }
         }
     }
--- DaCoder/Code/Tests/DaCoder.Data.Tests/FunctionalTests/LanguageScenarioTests.cs
+++ DaCoder/Code/Tests/DaCoder.Data.Tests/FunctionalTests/LanguageScenarioTests.cs
@@ -23,5 +23,47 @@ namespace DaCoder.Data.Tests.FunctionalTests
                 Assert.IsTrue(exists);
             }
         }
+
+        [TestMethod]
+        public void UpdateLanguageIsPersisted()
+        {
+            using (var businessContext = new BusinessContext())
+            {
+                var language = new Language
+                {
+                    Name = "LanguageName"
+                };
+
+                businessContext.AddNewLanguage(language);
+
+                language.Name = "New LanguageName";
+
+                businessContext.UpdateLanguage(language);
+
+                bool updated = businessContext.DataContext.Languages.Any(l => l.Id == language.Id && l.Name == language.Name);
+
+                Assert.IsTrue(updated);
+            }
+        }
+
+        [TestMethod]
+        public void DeleteLanguageIsPersisted()
+        {
+            using (var businessContext = new BusinessContext())
+            {
+                var language = new Language
+                {
+                    Name = "LanguageName"
+                };
+
+                businessContext.AddNewLanguage(language);
+
+                businessContext.DeleteLanguage(language);
+
+                bool notDeleted = businessContext.DataContext.Languages.Any(l => l.Id == language.Id);
+
+                Assert.IsFalse(notDeleted);
+            }
+        }
     }
 }
