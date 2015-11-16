6b0588044f32af6497f7631fe0d7c04caa7cef91
Simon <simonjonpedersen@gmail.com>
2015-11-16 13:06:37 +0100

    Added actioncommand to plugininstallerdialog
    Added actioncommand to plugininstallerdialog

---
+++ DaCoder/Code/Desktop Client/DaCoder.DesktopClient/ViewModels/PluginInstallerViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Windows;
+using DaCoder.Core;
+using DaCoder.DesktopClient.Views;
+
+namespace DaCoder.DesktopClient.ViewModels
+{
+    public class PluginInstallerViewModel : ViewModel
+    {
+        /// <summary>
+        /// Gets the command that makes it possible to add a new plugin.
+        /// </summary>
+        public ActionCommand AddPluginCommand
+        {
+            get { return new ActionCommand(p => (new PluginInstallerDialog()).ShowDialog()); }
+        }
+
+    }
+}
